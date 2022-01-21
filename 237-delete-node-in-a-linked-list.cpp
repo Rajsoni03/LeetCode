15cf6de20a6b7d8e1f724950c9ca54e07b8b5bf4
Rajkumar Soni <rajksoni029@gmail.com>
2022-01-21 20:02:22 +0530

    day 8

---
+++ 237-delete-node-in-a-linked-list.cpp
@@ -0,0 +1,38 @@
+237. Delete Node in a Linked List
+https://leetcode.com/problems/delete-node-in-a-linked-list/
+
+/*
+Example 1:
+Input: head = [4,5,1,9], node = 5
+Output: [4,1,9]
+Explanation: You are given the second node with value 5, the linked list should become 4 -> 1 -> 9 after calling your function.
+
+
+Example 2:
+Input: head = [4,5,1,9], node = 1
+Output: [4,5,9]
+Explanation: You are given the third node with value 1, the linked list should become 4 -> 5 -> 9 after calling your function.
+ 
+
+Constraints:
+The number of the nodes in the given list is in the range [2, 1000].
+-1000 <= Node.val <= 1000
+The value of each node in the list is unique.
+The node to be deleted is in the list and is not a tail node
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val = node->next->val;
+        node->next = node->next->next;     
+    }
+};
